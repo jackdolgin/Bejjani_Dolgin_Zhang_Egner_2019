bf26829afe70b03344121a8290e618898b10cea1
Jack Dolgin <jack.dolgin@duke.edu>
2019-04-01 17:55:37 -0400

    Add files via upload

--- Script_all_Exps.py
+++ Script_all_Exps.py
@@ -29,10 +29,11 @@ expInfo['frameRate'] = win.getActualFrameRate()
 #Create randomized list of cue conditions for different participants, counterbalanced; this randomly-generated list is used for all participants, hence the next two lines are commented out
 # cueversions = np.repeat([0,1,2,3,4,5],4)
 # np.random.shuffle(cueversions)
+
 if int(expInfo['session']) == 1:
-    cueversions = [3,0,3,3,2,3,1,4,2,5,0,5,4,2,5,1,0,4,2,5,4,1,0,1,5.0,1,3,2,4]
+    cueversions = [3,0,3,3,2,3,1,4,2,5,0,5,4,2,5,1,0,4,2,5,4,1,0,1,5.0,1,3,2,4,2,5,4,2,5]
 elif int(expInfo['session']) == 2:
-    cueversions = [1,2,5,4,1,5,4,0,0,1,4,4,0,2,3,2,3,3,5,1,0,5,3,2]
+    cueversions = [1,2,5,4,1,5,4,0,0,1,4,4,0,2,3,2,3,3,5,1,0,5,3,2,1,3,5,4,0,2]
 elif int(expInfo['session']) == 3:
     cueversions = [4,4,4,4,2,5,0,1,5,5,1,4,3,0,2,4,1,3,3,1,3,2,0,0,5,2]
 elif int(expInfo['session']) == 4:
