79192b93b748ef43efe920e2f9e70dda413d476d
Jack Dolgin <jack.dolgin@duke.edu>
2019-05-06 15:53:23 -0400

    Update Script_all_Exps.py

--- Script_all_Exps.py
+++ Script_all_Exps.py
@@ -33,14 +33,14 @@ expInfo['frameRate'] = win.getActualFrameRate()
 
 if int(expInfo['session']) == 1:
     cueversions = [3,0,3,3,2,3,1,4,2,5,0,5,4,2,5,1,0,4,2,5,4,1,0,1,5.0,1,3,2,4,
-                  2,5,4,2,5]
+                  2,5,4,2,5,3]
 elif int(expInfo['session']) == 2:
     cueversions = [1,2,5,4,1,5,4,0,0,1,4,4,0,2,3,2,3,3,5,1,0,5,3,2,1,3,5,4,0,2,
-                   0,2,4,1,3,5]
+                   0,2,4,1,3,5,3,4,0,1,2,1,4,0,3,4,2,1,5,1,3,5]
 elif int(expInfo['session']) == 3:
-    cueversions = [4,4,4,4,2,5,0,1,5,5,1,4,3,0,2,4,1,3,3,1,3,2,0,0,5,2]
+    cueversions = [4,4,4,4,2,5,0,1,5,5,1,4,3,0,2,4,1,3,3,1,3,2,0,0,5,2,3,4]
 elif int(expInfo['session']) == 4:
-    cueversions = [1,2,2,4,3,3,4,5,1,5,0,0,5,0,0,4,1,3,3,5,2,2,4,1]
+    cueversions = [1,2,2,2,3,3,4,5,1,5,0,0,2,0,0,4,1,3,3,5,4,5,4,1,0,2,4,1,3,5,4,2,3,1,0,5]
 
 CueVersion = cueversions[int(expInfo['participant'])%100 - 1]
 
