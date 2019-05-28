db878c8f394e12688029620abd8ac3b493d98320
Jack Dolgin <jack.dolgin@duke.edu>
2019-05-28 17:51:30 -0400

    Update Script_all_Exps.py

--- Script_all_Exps.py
+++ Script_all_Exps.py
@@ -33,14 +33,14 @@ expInfo['frameRate'] = win.getActualFrameRate()
 
 if int(expInfo['session']) == 1:
     cueversions = [3,0,3,3,2,3,1,4,2,5,0,5,4,2,5,1,0,4,2,5,4,1,0,1,5.0,1,3,2,4,
-                  2,5,4,2,5,3]
+                  2,5,4,2,5,3,0,3,1,2,5,4,5,1,5,4,4,2,3,0,4,1,2,0,3,5,3,5,2,1,4,0]
 elif int(expInfo['session']) == 2:
     cueversions = [1,2,5,4,1,5,4,0,0,1,4,4,0,2,3,2,3,3,5,1,0,5,3,2,1,3,5,4,0,2,
-                   0,2,4,1,3,5,3,4,0,1,2,1,4,0,3,4,2,1,5,1,3,5]
+                   0,2,4,1,3,5,3,4,0,1,2,1,4,0,3,4,2,1,5,1,3,5,0,3,5,4,2,1,3,0,5,2,1,4,4,3,5,0,2,1,5,1,4,3,0,2]
 elif int(expInfo['session']) == 3:
-    cueversions = [4,4,4,4,2,5,0,1,5,5,1,4,3,0,2,4,1,3,3,1,3,2,0,0,5,2,3,4]
+    cueversions = [4,4,4,4,2,5,0,1,5,5,1,4,3,0,2,4,1,3,3,1,3,2,0,0,5,2,3,4,0,1,3,4,2,5,2,1,3,0,5,4,1,4,2,3,0,5,1,3,2,4,0,5]
 elif int(expInfo['session']) == 4:
-    cueversions = [1,2,2,2,3,3,4,5,1,5,0,0,2,0,0,4,1,3,3,5,4,5,4,1,0,2,4,1,3,5,4,2,3,1,0,5]
+    cueversions = [1,2,2,2,3,3,4,5,1,5,0,0,2,0,0,4,1,3,3,5,4,5,4,1,0,2,4,4,1,3,5,4,2,3,1,0,5,4,2,1,3,5,4,3,0,2,1,5,1,2,0,4,3,4,3,0,2,5,1]            
 
 CueVersion = cueversions[int(expInfo['participant'])%100 - 1]
 
@@ -577,7 +577,6 @@ for rep in range(5 - (int(expInfo['session']))/3):
             event.clearEvents(eventType='keyboard')
             Instr1.setText(Post_Instr_2)
             Instr1.setAutoDraw(True)
-            print "hello"
             while (len(event.getKeys(keyList=["space"])) == 0):
                 win.flip()
         Instr1.setAutoDraw(False)
